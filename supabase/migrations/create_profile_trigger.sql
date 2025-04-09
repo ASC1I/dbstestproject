b3c74d814628eb8e63b52642d8a6d1768cdaef52
Faz <michaeljeffersonlee@gmail.com>
2025-04-09 13:21:40 -0400

    edited create_profile_trigger

--- supabase/migrations/create_profile_trigger.sql
+++ supabase/migrations/create_profile_trigger.sql
@@ -2,8 +2,8 @@
 create or replace function public.handle_new_user()
 returns trigger as $$
 begin
-  insert into public."Profile" (id, email, name)
-  values (new.id, new.email, new.raw_user_meta_data->>'name');
+  insert into public."Profile" (id, email)
+  values (new.id, new.email);
   return new;
 end;
 $$ language plpgsql security definer;
@@ -12,3 +12,4 @@ $$ language plpgsql security definer;
 create or replace trigger on_auth_user_created
   after insert on auth.users
   for each row execute procedure public.handle_new_user(); 
+
